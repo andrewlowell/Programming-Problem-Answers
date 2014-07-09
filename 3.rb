56935f8bca6cae75e2e351516407421750e3c30e
Lowell <mynock101@gmail.com>
2014-07-08 22:31:44 -0500

    Finished algorithm, but syntax off, need to fix.

--- 3.rb
+++ 3.rb
@@ -1,3 +1,16 @@
 prime_factors = []
-main_number = 8546217951
+main_number = 12
+working_number = main_number
+working_factor = 2
 
+while working_factor < working_number do
+  if working_number % working_factor == 0
+    prime_factors.push(working_factor)
+    working number /= working_factor    
+    working_factor = 2 unless working_factor == working_number
+  else
+    working_factor++
+  end
+end
+
+puts "The largest prime factor is " + prime_factors.max
