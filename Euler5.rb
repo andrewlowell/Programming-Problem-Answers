d8adaa2486c6a1b901a332a7aaaa63455b7870a0
Lowell <mynock101@gmail.com>
2014-07-10 00:04:56 -0500

    Various.

--- Euler5.rb
+++ Euler5.rb
@@ -1,17 +1,17 @@
 divisible_numbers = []
 divisors = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11]
 finished = false
-working_number = 0
+working_number = 21 
 unless finished
   divisible_answers = []
-  for x in divisors
-    if working_number % x == 0
+  for i in (0..divisors.length - 1)
+    if working_number % divisors[i] == 0
       divisible_answers.push(true)
     else
       divisible_answers.push(false)
     end
   end
-  if divisible_answers.include? false
+  if divisible_answers.include?(false)
     working_number += 1
   else
     puts "The smallest evenly divisible number is: " + "#{working_number}"
