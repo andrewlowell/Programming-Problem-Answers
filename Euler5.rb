65ef050e3f4d1fb9a3a952be892f5395660d6b6a
Lowell <mynock101@gmail.com>
2014-07-11 00:58:30 -0500

    An answer for Euler5 that works, but takes a prohibitively long time.

--- Euler5.rb
+++ Euler5.rb
@@ -1,18 +1,22 @@
 divisible_numbers = []
 divisors = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11]
 finished = false
-working_number = 21 
-unless finished
+working_number = 20
+while (!finished)
   divisible_answers = []
   for i in (0..divisors.length - 1)
     if working_number % divisors[i] == 0
       divisible_answers.push(true)
+      puts "#{working_number} is divisible by #{divisors[i]}."
     else
       divisible_answers.push(false)
+      puts "#{working_number} is not divisible by #{divisors[i]}."
+      break
     end
   end
   if divisible_answers.include?(false)
-    working_number += 1
+    working_number += 20
+    puts "We need to go up a working number! Now: #{working_number}"
   else
     puts "The smallest evenly divisible number is: " + "#{working_number}"
     finished = true
