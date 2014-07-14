77d4b5e2bd1659d96d529328d7f310dff7642e81
Lowell <mynock101@gmail.com>
2014-07-13 22:06:24 -0500

    Copied some code which will help build the fast answer to Euler5.

--- Euler5.rb
+++ Euler5.rb
@@ -1,9 +1,21 @@
+n_number = gets.to_i
+working_number = main_number
+working_factor = 2
 
-
-
-
-
-
+while working_factor <= working_number
+  if working_number % working_factor == 0
+    prime_factors.push(working_factor)
+    working_number = working_number / working_factor
+    puts "Found prime factor: " + "#{working_factor}"
+    if working_factor == working_number
+      working_factor = working number + 1
+    else
+      working_factor = 2
+    end
+  else
+    working_factor += 1
+  end
+end
 
 
 
